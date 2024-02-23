81843ab863cd2ecbc272956fce8f2a8a48ae2d02
Nicholas <hongyu.chen@funplus.com>
2024-02-23 19:09:28 +0800

    fix: styles fixes

--- wp-content/themes/funplus-theme/template-parts/sections/careers_list.php
+++ wp-content/themes/funplus-theme/template-parts/sections/careers_list.php
@@ -8,23 +8,23 @@
 // Content
 
 if (isset($_GET["location"])) {
-    $location = filter_var($_GET["location"]);
-    $tax_query = array (
-        array(
-            'taxonomy' => 'country',
-            'field' => 'slug',
-            'terms' => $location,
-        )
-    );
+  $location = filter_var($_GET["location"]);
+  $tax_query = array(
+    array(
+      'taxonomy' => 'country',
+      'field' => 'slug',
+      'terms' => $location,
+    )
+  );
 } else {
-    $location = 'All Locations';
-    $tax_query = '';
+  $location = 'All Locations';
+  $tax_query = '';
 }
 
 $careers = new WP_Query([
-    'post_type'      => 'career',
-    'posts_per_page' => 6,
-    'tax_query' => $tax_query
+  'post_type'      => 'career',
+  'posts_per_page' => 6,
+  'tax_query' => $tax_query
 ]);
 
 // Load More
@@ -34,19 +34,36 @@ $loadMore_id = str_replace('.', '', uniqid('loadMore_'));
 ?>
 
 <section class="career-list post-loop" id="<?php echo esc_attr($loadMore_id); ?>" data-max="<?php echo $max ?>">
-    
-    <!-- Career List -->
-    <div class="container container-sm">
-        <div class="jv-careersite" data-careersite="funplus" data-force-redirect></div>
-    </div>
+
+  <!-- Career List -->
+  <div class="container container-sm">
+    <?php
+    // $response = wp_remote_get(
+    //   "https://api.factorialhr.com/api/v1/ats/job_postings",
+    //   [
+    //     'headers' => [
+    //       'Content-Type' => 'application/json',
+    //       'X-Api-Key' => '2648e57deebccbbddb8acaa2548bbd08e138b50105999d6285c020a964ed3d52'
+    //     ]
+    //   ]
+    // );
+    // $jobs = json_decode($response['body']);
+    // // debug($jobs);
+    // foreach ($jobs as $job) {
+    //   echo $job->title;
+    //   echo PHP_EOL;
+    // }
+    ?>
+    <iframe width="100%" height="800" src="https://funplus.factorialhr.com/embed/jobs" frameborder="0"></iframe>
+  </div>
 </section>
 
 
 
-<script src="https://jobs.jobvite.com/__assets__/scripts/careersite/public/iframe.js"></script>
+<!-- <script src="https://jobs.jobvite.com/__assets__/scripts/careersite/public/iframe.js"></script> -->
 
-<script type="text/javascript">
+<!-- <script type="text/javascript">
     jQuery(document).ready(() => {
         initLoadMore('<?php echo $loadMore_id; ?>');
     });
-</script>
+</script> -->
