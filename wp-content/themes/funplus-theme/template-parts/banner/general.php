3caa0fcff1d4d0a768f6d5246684c58923f7c2ae
hongyu.chen <hongyu.chen@funplus.com>
2024-07-24 18:05:09 +0800

    comic update

--- wp-content/themes/funplus-theme/template-parts/banner/general.php
+++ wp-content/themes/funplus-theme/template-parts/banner/general.php
@@ -8,6 +8,8 @@
 $text = get_field('banner_text');
 $button = get_field('banner_button');
 $bg_image = get_field('banner_img');
+$extra_logo = get_field('extra_logo');
+$extra_logo_link = get_field('extra_logo_link');
 $banner_id = str_replace('.', '', uniqid('banner_'));
 
 // Height
@@ -16,34 +18,41 @@ $height = empty($bg_image) ? 'fixed-height' : '';
 ?>
 
 <div class="banner-container" id="<?= $banner_id; ?>">
-  <div class="banner wow">
+    <div class="banner wow">
 
-    <div class="container banner-inner-container">
-      <div class="banner__inner">
-        <div class="banner__main fixed-height">
-          <div class="banner__main__content">
-            <?php get_template_part('template-parts/banner/parts/heading'); ?>
+        <div class="container banner-inner-container">
+            <div class="banner__inner">
+                <div class="banner__main fixed-height">
+                    <div class="banner__main__content">
+                        <div class="banner__extra_logo">
+                            <a href="<?php echo $extra_logo_link; ?>">
+                                <?php if (!empty($extra_logo)) : ?>
+                                    <?= wp_get_attachment_image($extra_logo, 'full', false, ['class' => 'fadeIn']); ?>
+                                <?php endif; ?>
+                            </a>
+                        </div>
+                        <?php get_template_part('template-parts/banner/parts/heading'); ?>
 
-            <?php if (!empty($text)) : ?>
-              <div class="banner-blurb fadeIn">
-                <div class="row">
-                  <div class="col-12">
-                    <?= $text; ?>
-                  </div>
+                        <?php if (!empty($text)) : ?>
+                            <div class="banner-blurb fadeIn">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <?= $text; ?>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php endif; ?>
+                    </div>
+                    <div class="banner__main__feature">
+                        <?php if (!empty($bg_image)) : ?>
+                            <?= wp_get_attachment_image($bg_image, 'full', false, ['class' => 'fadeIn']); ?>
+                        <?php endif; ?>
+                    </div>
+
                 </div>
-              </div>
-            <?php endif; ?>
-          </div>
-          <div class="banner__main__feature">
-            <?php if (!empty($bg_image)) : ?>
-              <?= wp_get_attachment_image($bg_image, 'full', false, ['class' => 'fadeIn']); ?>
-            <?php endif; ?>
-          </div>
-
+            </div>
         </div>
-      </div>
-    </div>
 
-  </div>
-  <!-- </div> -->
+    </div>
+    <!-- </div> -->
 </div>
