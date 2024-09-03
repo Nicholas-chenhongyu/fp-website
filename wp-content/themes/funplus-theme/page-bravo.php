2cfc32e922e7a2e94137f5d6d38556637a79ed55
hongyu.chen <hongyu.chen@funplus.com>
2024-09-03 17:33:14 +0800

    feat: 新增网红平台入口页面

---
+++ wp-content/themes/funplus-theme/page-bravo.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Template Name: Bravo Games
+ */
+
+
+get_header();
+?>
+
+<main class="content-wrapper blank">
+
+    <iframe id="bravo" width="100%" height="1080px" src="https://bravo-games.com/externalHome" frameborder="0"></iframe>
+
+    <script>
+        window.addEventListener(
+            "message",
+            (event) => {
+                if (event.origin !== "https://bravo-games.com") return;
+
+                const ratio = event.data.height / event.data.width;
+                document.querySelector('#bravo').setAttribute("height", window.innerWidth * ratio + 'px')
+            },
+            false,
+        );
+    </script>
+
+    <?php
+    if (have_posts()) :
+        while (have_posts()) :
+            the_post();
+
+            get_template_part('template-parts/sections');
+        endwhile;
+    endif;
+    ?>
+
+</main>
+
+<?php get_footer(); ?>
--- wp-content/themes/funplus-theme/template-parts/sections/bookshelf.php
+++ wp-content/themes/funplus-theme/template-parts/sections/bookshelf.php
@@ -1,2 +1,4 @@
-<book-shelf bookshelfId="FC9ibjWNzvEnzHUz"></book-shelf>
-<script type="module" crossorigin src="https://bookshelf-read.funplus.com/latest/funplus-bookshelf.js"></script>
+<div class="container container-sm">
+    <book-shelf bookshelfId="FC9ibjWNzvEnzHUz"></book-shelf>
+    <script type="module" crossorigin src="https://bookshelf-read.funplus.com/latest/funplus-bookshelf.js"></script>
+</div>
