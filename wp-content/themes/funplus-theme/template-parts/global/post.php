7acfcff0b7694893b189c3f187745d9601545922
hongyu.chen <hongyu.chen@funplus.com>
2024-11-22 10:38:34 +0800

    fix: style fixes

--- wp-content/themes/funplus-theme/template-parts/global/post.php
+++ wp-content/themes/funplus-theme/template-parts/global/post.php
@@ -4,13 +4,13 @@
  * The post loop
  */
 
-$thumbnail = has_post_thumbnail() ? get_post_thumbnail_id() : get_field('default_post_thumbnail', 'option'); 
+$thumbnail = has_post_thumbnail() ? get_post_thumbnail_id() : get_field('default_post_thumbnail', 'option');
 $externalUrl = get_field('external_url');
 ?>
 
-<div class="col-12 col-md-6 col-lg-4">
-    <a class="post-loop__post wow fadeIn" target="_blank" href="<?= ! empty($externalUrl) ? $externalUrl : get_the_permalink(); ?>" <?= ! empty($externalUrl) ? 'rel="noreferrer noopener"': ''; ?>>
+<div class="col-12 col-md-6 col-lg-4 press">
+    <a class="post-loop__post wow fadeIn" target="_blank" href="<?= ! empty($externalUrl) ? $externalUrl : get_the_permalink(); ?>" <?= ! empty($externalUrl) ? 'rel="noreferrer noopener"' : ''; ?>>
         <h4 class="post-loop__post__heading"><?php echo get_the_title(); ?></h4>
-        <p class="post-loop__post__excerpt"><?php echo get_the_excerpt(); ?></p>
     </a>
+    <p class="post-loop__post__excerpt date"><?php echo get_the_date(); ?></p>
 </div>
