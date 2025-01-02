f4e6f941ecb2ce98109b54480f80365bd8aabb11
hongyu.chen <hongyu.chen@funplus.com>
2025-01-02 19:27:54 +0800

    feat: change bookshelf style

--- wp-content/themes/funplus-theme/template-parts/sections/bookshelf.php
+++ wp-content/themes/funplus-theme/template-parts/sections/bookshelf.php
@@ -6,7 +6,7 @@ $show = get_sub_field('show_this_bookshelf');
 
     <div class="container container-sm">
         <?php if ($show) : ?>
-            <book-shelf bookshelfId="FC9ibjWNzvEnzHUz"></book-shelf>
+            <book-shelf bookshelfId="FC9ibjWNzvEnzHUz" theme="slider"></book-shelf>
             <script type="module" crossorigin src="https://bookshelf-read.funplus.com/latest/funplus-bookshelf.js"></script>
         <?php endif; ?>
     </div>
