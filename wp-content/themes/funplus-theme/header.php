3b1d0e6ca44ea1c1b68b7813d0a58813bb33ebf3
hongyu.chen <hongyu.chen@funplus.com>
2025-04-16 18:06:27 +0800

    add matomo

--- wp-content/themes/funplus-theme/header.php
+++ wp-content/themes/funplus-theme/header.php
@@ -39,6 +39,27 @@
     </script>
     <!-- End Google Tag Manager -->
 
+    <!-- Matomo -->
+    <script>
+        var _paq = window._paq = window._paq || [];
+        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+            var u = "//analytics-read.funplus.com/";
+            _paq.push(['setTrackerUrl', u + 'matomo.php']);
+            _paq.push(['setSiteId', '2']);
+            var d = document,
+                g = d.createElement('script'),
+                s = d.getElementsByTagName('script')[0];
+            g.async = true;
+            g.src = u + 'matomo.js';
+            s.parentNode.insertBefore(g, s);
+        })();
+    </script>
+    <!-- End Matomo Code -->
+
+
     <?php wp_head(); ?>
 </head>
 
