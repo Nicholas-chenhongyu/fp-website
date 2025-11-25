c25adc478a324944b1bfac5d57b06b3a6713f492
hongyu.chen <hongyu.chen@funplus.com>
2025-11-25 10:42:09 +0800

    Add form only template

---
+++ wp-content/themes/funplus-theme/page-form-only.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Template Name: Form Only Page
+ */
+
+
+get_header();
+$disable_bullet = get_field('disable_bullet_styles');
+?>
+
+<main class="content-wrapper blank">
+
+    <?php
+    if ($disable_bullet) {
+    ?>
+        <style>
+            ul li {
+                list-style: none !important;
+            }
+        </style>
+    <?php
+    }
+    ?>
+    <?php
+    if (have_posts()) :
+        while (have_posts()) :
+            the_post();
+
+            get_template_part('template-parts/sections');
+        endwhile;
+    endif;
+    ?>
+
+</main>
+
+<?php get_footer(); ?>
